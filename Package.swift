2dc5b0fd21a2a3181355911825b3568d1b665bc7
Daniel Stevens <65318437+dboone323@users.noreply.github.com>
2026-02-19 09:16:03 -0600

    Finalize project upgrades: Swift 6.2 / iOS 18.0 target and fixes

--- Package.swift
+++ Package.swift
@@ -7,8 +7,8 @@ import PackageDescription
 let package = Package(
     name: "MomentumFinance",
     platforms: [
-        .iOS(.v17),
-        .macOS(.v14),
+        .iOS(.v18),
+        .macOS(.v15),
     ],
     products: [
         .library(
